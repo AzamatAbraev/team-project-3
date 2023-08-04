39faf550359646351707aae9de75ece863c531d8
AzamatAbraev <azamatabraev07@gmail.com>
2023-08-04 16:56:30 +0500

    structure and code samples added

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./pages/*.html", "./components/*.{html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
