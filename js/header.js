6a0384eb71dcb6e653d3ca834089f508476539e5
AzamatAbraev <azamatabraev07@gmail.com>
2023-08-05 12:29:33 +0500

    header added

---
+++ js/header.js
@@ -0,0 +1,13 @@
+function openNavbar() {
+  document.getElementById("navbar-responsive").style.right = "0";
+  document.getElementById("navbar-open").style.zIndex = "-1";
+  // document.getElementById("navbar-responsive").style.top = "0";
+}
+function closeNavbar() {
+  document.getElementById("navbar-responsive").style.right = "-100%";
+  document.getElementById("navbar-open").style.zIndex = "100";
+  // document.getElementById("navbar-responsive").style.top = "-100%";
+}
+
+document.getElementById("navbar-open").addEventListener("click", openNavbar);
+document.getElementById("navbar-close").addEventListener("click", closeNavbar);
